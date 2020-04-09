ef99dff2c42399bdc3836b7ff1724ab23f21b93b
Josh <jmillikan12345@gmail.com>
2020-04-09 11:57:52 -0400

    handled nulls

--- src/main/java/minesweeper/Tile.java
+++ src/main/java/minesweeper/Tile.java
@@ -3,7 +3,7 @@ package minesweeper;
 public class Tile {
     public final boolean isMine;
     private final int x,y;
-    private int NumberOfAdjacentMines = 0;
+    private byte NumberOfAdjacentMines = 0;
     private boolean isRevealed = false;
 
     Tile(final int mineChance, final int x, final int y) {
@@ -24,14 +24,18 @@ public class Tile {
 
     void Scan() {
         var game = Main.game;
-        if(game != null) { //todo handle nulls
-            if(game.getTile(x+1,y).isMine)
+        if(game != null) {
+            var tile = game.getTile(x+1,y);
+            if(tile != null && tile.isMine)
                 NumberOfAdjacentMines++;
-            if(game.getTile(x-1,y).isMine)
+            tile = game.getTile(x-1,y);
+            if(tile != null && tile.isMine)
                 NumberOfAdjacentMines++;
-            if(game.getTile(x,y+1).isMine)
+            tile = game.getTile(x,y+1);
+            if(tile != null && tile.isMine)
                 NumberOfAdjacentMines++;
-            if(game.getTile(x,y-1).isMine)
+            tile = game.getTile(x,y-1);
+            if(tile != null && tile.isMine)
                 NumberOfAdjacentMines++;
         }
     }
